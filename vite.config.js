8717f210ccaff9c72735039c7654800f8b3cd28a
wesley <wesley.cursos2024@gmail.com>
2025-11-04 20:20:52 -0300

    build funcionando

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
-  }
+    port: 5173,
+  },
+  resolve: {
+    alias: {
+      'react-is': 'react-is/index.js',
+    },
+  },
 })
