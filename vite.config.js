e22528eb1c4bc6c1b223aafa4181f3023eaf7e0b
wesley <wesley.cursos2024@gmail.com>
2025-11-04 22:33:30 -0300

    Corrigindo permissões vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['react-is']
-  }
-})
-
+  build: {
+    rollupOptions: {
+      output: {
+        // Cria um chunk separado para React/ReactDOM e outras dependências grandes
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Coloca todas as dependências em um chunk chamado 'vendor'
+            // Você pode refinar isso para criar chunks separados para 'recharts', 'bootstrap', etc.
+            return 'vendor'; 
+          }
+        },
+      },
+    },
+  },
+});
